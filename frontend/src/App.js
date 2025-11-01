ab66c3d700bca7983247a36aa19ff47b10293a93
ultburner00001 <ultburner.00001@gmail.com>
2025-11-01 15:23:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,29 @@
-// frontend/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import './App.css';
+
+// Pages
 import Home from './pages/Home';
 import Notes from './pages/Notes';
 import Courses from './pages/Courses';
 import AskDoubt from './pages/AskDoubt';
-import Auth from './pages/Auth';
 import Timetable from './pages/Timetable';
 import Admin from './pages/Admin';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route 
-              path="/notes" 
-              element={
-                <ProtectedRoute>
-                  <Notes />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="/courses" element={<Courses />} />
-            <Route 
-              path="/ask-doubt" 
-              element={
-                <ProtectedRoute>
-                  <AskDoubt />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/timetable" 
-              element={
-                <ProtectedRoute>
-                  <Timetable />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/ask-doubt" element={<AskDoubt />} />
+          <Route path="/timetable" element={<Timetable />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
