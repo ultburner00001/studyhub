cf4b08837139905fe6baeb72b466c23fc9661668
ultburner00001 <ultburner.00001@gmail.com>
2025-11-01 16:22:14 +0530

    Update Timetable.js

--- backend/models/Timetable.js
+++ backend/models/Timetable.js
@@ -1,37 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const timetableSchema = new mongoose.Schema({
-  schedule: [{
-    day: {
-      type: String,
-      enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      required: true
-    },
-    slots: [{
-      time: {
-        type: String,
-        required: true
-      },
-      subject: {
-        type: String,
-        required: true
-      },
-      topic: {
-        type: String
-      },
-      isCompleted: {
-        type: Boolean,
-        default: false
-      }
-    }]
-  }],
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }
-}, {
-  timestamps: true
+  day: { type: String, required: true },
+  subject: { type: String, required: true },
+  time: { type: String, required: true },
+  teacher: { type: String },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Timetable', timetableSchema);
+export default mongoose.model("Timetable", timetableSchema);
