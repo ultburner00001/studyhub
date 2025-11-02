7229ec0cf55c5f4313db5eaf70677d63cb396eb7
ultburner00001 <ultburner.00001@gmail.com>
2025-11-02 14:40:15 +0530

    Update Doubt.js

--- backend/models/Doubt.js
+++ backend/models/Doubt.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
-const doubtSchema = new mongoose.Schema({
-  name: { type: String },
-  question: { type: String, required: true },
-  subject: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
+const doubtSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    question: { type: String, required: true, trim: true },
+    subject: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Doubt", doubtSchema);
