ddbaffc94241cdb71000c4ec900154ec3b87a068
ultburner00001 <ultburner.00001@gmail.com>
2025-11-10 08:20:23 +0530

    Add minimal AuthProvider and wrap App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
